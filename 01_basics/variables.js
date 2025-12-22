54614c0aaedce3a8e503d0654eeabfd7138beba3
Deepak kumar <84015059+Deepakkr44@users.noreply.github.com>
2025-12-22 09:46:29 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 123456
+let accountEmail = "deepakkumar44@gmail.com"
+var accountPassword= "121212"
+accountCity= "jaipur"
+let accountState;
+
+//accountId=2 // not allowed
+
+accountEmail = "delhi@gmail.com"
+accountPassword= "212121"
+accountCity = "bengluru"
+
+console.log(accountId);
+
+console.table ([accountId, accountEmail, accountCity, accountPassword, accountState]
+
+)
