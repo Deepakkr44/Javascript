4a2680e1310925671d43faaaf2581213b19785ed
Deepak kumar <84015059+Deepakkr44@users.noreply.github.com>
2025-12-23 13:59:06 +0000

    Conversion of datatypes

---
+++ 01_basics/conversionOperators.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber= Number(score)
+
+//console.log(typeof valueInNumber );
+//console.log(valueInNumber);
+
+//let isLoggedIn = 1
+
+//let booleanIsLoggedIn =Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => False
+
+// "" => false 
+//"Deepak" => True
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+
+
+
+
